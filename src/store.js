c9d991ef191a7bac6fa0ce556be48e9fc9536080
ŁukaszKubok <syrathea@gmail.com>
2019-07-18 21:59:59 +0200

    Removed Redux Devtools from store

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import thunk from "redux-thunk";
 export default createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
