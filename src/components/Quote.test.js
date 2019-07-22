ae71ea8b3637b592e9180ef42796588d2bae0125
ŁukaszKubok <syrathea@gmail.com>
2019-07-22 16:06:42 +0200

    Added testing files

---
+++ src/components/Quote.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Quote from "./Quote";
+describe("MyComponent", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<Quote debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
